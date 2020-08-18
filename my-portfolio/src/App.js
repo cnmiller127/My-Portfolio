dd5a8a31a7788227fa48f71e3467701f6f5ea3fa
Christian Miller <cnmiller127@gmail.com>
2020-08-17 23:06:36 -0500

    Hashrouter

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Router as Router, Route } from "react-router-dom";
+import { Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import About from "./components/About";
 import Portfolio from "./components/Portfolio";
@@ -12,17 +12,17 @@ import Footer from './components/Footer';
 
 function App() {
   return (
-    <Router>
+    
       <div className = "container-fluid m-0 p-0 bg">
         <Navbar />
         <Route exact path="/" component={Portfolio} />
-        <Route exact path="/about" component={About} />
-        <Route exact path="/portfolio" component={Portfolio} />
-        <Route exact path="/contact" component={Contact} />
+        <Route path="/about" component={About} />
+        <Route path="/portfolio" component={Portfolio} />
+        <Route path="/contact" component={Contact} />
         <Spacer/>
         <Footer/>
       </div>
-    </Router>
+    
   );
 }
 
