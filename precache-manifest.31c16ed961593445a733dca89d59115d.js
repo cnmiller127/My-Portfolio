0f48d43a7b99f8b2693900d68984728fff95f11f
Christian Miller <cnmiller127@gmail.com>
2020-08-17 20:23:59 -0500

    Updates

---
+++ precache-manifest.31c16ed961593445a733dca89d59115d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d54e0d161b88eeb08e91aad9d861d30",
+    "url": "/My-Portfolio/index.html"
+  },
+  {
+    "revision": "c91d249845885543d310",
+    "url": "/My-Portfolio/static/css/main.0e371ad9.chunk.css"
+  },
+  {
+    "revision": "94fa5c085c9e7bdadf5c",
+    "url": "/My-Portfolio/static/js/2.d9516649.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/My-Portfolio/static/js/2.d9516649.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c91d249845885543d310",
+    "url": "/My-Portfolio/static/js/main.e7478f5c.chunk.js"
+  },
+  {
+    "revision": "cc8fb5c09ef196e1df09",
+    "url": "/My-Portfolio/static/js/runtime-main.2dca773a.js"
+  },
+  {
+    "revision": "4a585bb7808c93152514479971764bec",
+    "url": "/My-Portfolio/static/media/bg.4a585bb7.jpg"
+  },
+  {
+    "revision": "3908a5ae3be2799c0f6859e7ebe3510e",
+    "url": "/My-Portfolio/static/media/date.3908a5ae.png"
+  },
+  {
+    "revision": "1e2755512e06f81673a8031670c48b69",
+    "url": "/My-Portfolio/static/media/fitness.1e275551.png"
+  },
+  {
+    "revision": "603a063ae769343d4522f9cee5de20a0",
+    "url": "/My-Portfolio/static/media/quiz.603a063a.png"
+  },
+  {
+    "revision": "d005310b96ef61c4f29747be289c6f67",
+    "url": "/My-Portfolio/static/media/wthr.d005310b.png"
+  }
+]);
